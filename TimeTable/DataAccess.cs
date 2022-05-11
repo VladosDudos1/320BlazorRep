a6ea043a28126ca7556998559ca47da6654820da
Михаил Мясников <gutufik@mail.ru>
2022-05-11 21:51:16 +0300

    Added DataAccess

---
+++ TimeTable/DataAccess.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Driver;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace TimeTable
+{
+    public class DataAccess
+    {
+        public static MongoClient client = new MongoClient("mongodb://localhost");
+        public static MongoDatabaseBase database = (MongoDatabaseBase)client.GetDatabase("TimeTable");
+
+        public static List<TimeTableDay> GetTimeTable()
+        {
+            var timeTable = (MongoCollectionBase<TimeTableDay>)database.GetCollection<TimeTableDay>("TimeTable");
+            return timeTable.Find(x => true).ToList();
+        }
+
+        public static void SaveTimeTableDay(TimeTableDay day)
+        {
+            var timeTable = (MongoCollectionBase<TimeTableDay>)database.GetCollection<TimeTableDay>("TimeTable");
+            if (GetTimeTable().FirstOrDefault(x => x.Id == day.Id) != null)
+            {
+                var filter = Builders<TimeTableDay>.Filter.Eq(s => s.Id, day.Id);
+                timeTable.ReplaceOneAsync(filter, day);
+            }
+            else
+                timeTable.InsertOneAsync(day);
+        }
+    }
+}
