ee0ae74efd82cd9222e29f049c02d88b455fdb6f
Nasurtdinova <nasurtdinovag@mail.ru>
2022-05-12 09:24:29 +0300

    Add classes timetable

--- BlazorProject/Data/Timetable.cs
+++ BlazorProject/Data/Timetable.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MongoDB.Bson;
 
 namespace BlazorProject.Data
 {
     public class Timetable
     {
+        public ObjectId Id { get; set; }
+        public string NameTeacher { get; set; }
+        public string Subject { get; set; }
+        public string Classroom { get; set; }
     }
 }
---
+++ BlazorProject/Data/TimetableService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace BlazorProject.Data
+{
+    public class TimetableService
+    {
+        public List<string> GetDay()
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("Timetable");
+            return database.ListCollectionNames().ToList();
+        }
+
+        public async Task<List<Timetable>> GetItem(string searchDay)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("Timetable");
+            if (database.ListCollectionNames().ToList().Exists(x => x == searchDay))
+            {
+                if (string.IsNullOrEmpty(searchDay))
+                    return null;
+                else
+                {
+                    var collection = database.GetCollection<Timetable>(searchDay);
+                    return await collection.Find(x => true).ToListAsync();
+                }
+            }
+            else
+                return null;
+        }
+    }
+}
