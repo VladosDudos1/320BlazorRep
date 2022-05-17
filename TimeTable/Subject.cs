b0a246203741d8f9bbc4b3b74e8a457eea1a16a7
JustMe17Artem <sematkinartem6@gmail.com>
2022-05-17 14:06:42 +0300

    initial

---
+++ TimeTable/Subject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TimeTable
+{
+    public class Subject
+    {
+        [BsonId]
+        [BsonIgnoreIfDefault]
+        private ObjectId _id { get; set; }
+        public ObjectId Id { get { return _id; } }
+        public string Name { get; set; }
+        public string Teacher { get; set; }
+    }
+}
---
+++ TimeTable/TimeTableDay.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TimeTable
+{
+    public class TimeTableDay
+    {
+        [BsonId]
+        [BsonIgnoreIfDefault]
+        private ObjectId _id { get; set; }
+        public ObjectId Id { get { return _id; } }
+        public string DayName { get; set; }
+        public List<Subject> Subjects { get; set; }
+        public TimeTableDay()
+        {
+            DayName = "";
+            Subjects = new List<Subject>();
+
+        }
+    }
+}
