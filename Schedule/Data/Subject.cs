f4becaf378fa1bb0b6fd8713bbf79f8d771c48fc
Даниил <dragon0lordchik@gmail.com>
2022-05-19 08:56:33 +0300

    Work is done

---
+++ Schedule/Data/Subject.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Schedule
+{
+    public class Subject
+    {
+        [BsonId]
+        [BsonIgnoreIfDefault]
+        private ObjectId _id { get; set; }
+        public ObjectId Id { get { return _id; } }
+        public string Name { get; set; }
+        public string Teacher { get; set; }
+    }
+}
---
+++ Schedule/Data/TimeTableDay.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+namespace Schedule
+{
+    public class TimeTableDay
+    {
+        [BsonId]
+        [BsonIgnoreIfDefault]
+        private ObjectId _id { get; set; }
+        public ObjectId Id { get { return _id; } }
+        public string DayName { get; set; }
+        public List<Subject> Subjects { get; set; }
+
+    }
+}
