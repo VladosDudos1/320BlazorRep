910a1e6be2db2c72b04e05e2c83c0e1537765d8a
Михаил Мясников <gutufik@mail.ru>
2022-05-19 08:32:32 +0300

    edited design

--- TimeTable/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/TimeTablePage.razor.g.cs
+++ TimeTable/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/TimeTablePage.razor.g.cs
@@ -13,63 +13,63 @@ namespace TimeTable.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "D:\Users\gutuf\source\repos\320BlazorRep\TimeTable\_Imports.razor"
+#line 1 "C:\Users\201913\Source\Repos\320BlazorRep\TimeTable\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\Users\gutuf\source\repos\320BlazorRep\TimeTable\_Imports.razor"
+#line 2 "C:\Users\201913\Source\Repos\320BlazorRep\TimeTable\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\Users\gutuf\source\repos\320BlazorRep\TimeTable\_Imports.razor"
+#line 3 "C:\Users\201913\Source\Repos\320BlazorRep\TimeTable\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\Users\gutuf\source\repos\320BlazorRep\TimeTable\_Imports.razor"
+#line 4 "C:\Users\201913\Source\Repos\320BlazorRep\TimeTable\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\Users\gutuf\source\repos\320BlazorRep\TimeTable\_Imports.razor"
+#line 5 "C:\Users\201913\Source\Repos\320BlazorRep\TimeTable\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\Users\gutuf\source\repos\320BlazorRep\TimeTable\_Imports.razor"
+#line 6 "C:\Users\201913\Source\Repos\320BlazorRep\TimeTable\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "D:\Users\gutuf\source\repos\320BlazorRep\TimeTable\_Imports.razor"
+#line 7 "C:\Users\201913\Source\Repos\320BlazorRep\TimeTable\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "D:\Users\gutuf\source\repos\320BlazorRep\TimeTable\_Imports.razor"
+#line 8 "C:\Users\201913\Source\Repos\320BlazorRep\TimeTable\_Imports.razor"
 using TimeTable;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\Users\gutuf\source\repos\320BlazorRep\TimeTable\Pages\TimeTablePage.razor"
+#line 3 "C:\Users\201913\Source\Repos\320BlazorRep\TimeTable\Pages\TimeTablePage.razor"
 using TimeTable.Shared;
 
 #line default
@@ -84,7 +84,7 @@ using TimeTable.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 21 "D:\Users\gutuf\source\repos\320BlazorRep\TimeTable\Pages\TimeTablePage.razor"
+#line 21 "C:\Users\201913\Source\Repos\320BlazorRep\TimeTable\Pages\TimeTablePage.razor"
        
     string selectedDay = "Monday";
 
@@ -96,12 +96,14 @@ using TimeTable.Shared;
 
         DataAccess.SaveTimeTableDay(new TimeTableDay
         {
-            DayName = "Sunday",
+            DayName = "Thursday",
             Subjects =
             new List<Subject>
             {
                     new Subject {Name = "MDK 01 04", Teacher = "Nacharov" },
+                    new Subject {Name = "Geometry", Teacher = "Nacharov" },
                     new Subject {Name = "MDK 01 04", Teacher = "Nacharov" },
+                    new Subject {Name = "Mathematic", Teacher = "Nacharov" },
             }
 
         }
