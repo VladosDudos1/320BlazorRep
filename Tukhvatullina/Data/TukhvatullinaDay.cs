fe0a9626ff04539ab04c02003e4c4e81912339a8
gulnaz.tukhvatullina.02@inbox.ru <gulnaz.tukhvatullina.02@inbox.ru>
2022-05-31 13:16:55 +0300

    finish

---
+++ Tukhvatullina/Data/TukhvatullinaDay.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+namespace Tukhvatullina.Data
+{
+    public class TukhvatullinaDay
+    {
+        [BsonId]
+        [BsonIgnoreIfDefault]
+        private ObjectId _id { get; set; }
+        public ObjectId Id { get => _id; }
+        public string DayName { get; set; }
+        public List<Subject> Subjects { get; set; }
+
+        public TukhvatullinaDay()
+        {
+            Subjects = new List<Subject>();
+        }
+    }
+}
---
+++ Tukhvatullina/Services/TukhvatullinaService.cs
@@ -0,0 +1,58 @@
+﻿using MongoDB.Driver;
+using Tukhvatullina.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tukhvatullina.Services
+{
+    public class TukhvatullinaService
+    {
+        public static MongoClient client = new MongoClient("mongodb://localhost");
+        public static MongoDatabaseBase database = (MongoDatabaseBase)client.GetDatabase("Schedule");
+
+        public List<TukhvatullinaDay> GetSchedule()
+        {
+            var schedule = (MongoCollectionBase<TukhvatullinaDay>)database.GetCollection<TukhvatullinaDay>("Schedule");
+            return schedule.Find(x => true).ToList();
+        }
+
+        public List<string> GetDays()
+        {
+            return Enumerable.Range(0, 7).Select(i => ((DayOfWeek)i).ToString()).ToList();
+        }
+
+        public void SaveTukhvatullinaDay(TukhvatullinaDay day)
+        {
+            var schedule = (MongoCollectionBase<TukhvatullinaDay>)database.GetCollection<TukhvatullinaDay>("Schedule");
+            if (GetSchedule().FirstOrDefault(x => x.Id == day.Id) != null)
+            {
+                var filter = Builders<TukhvatullinaDay>.Filter.Eq(s => s.Id, day.Id);
+                schedule.ReplaceOneAsync(filter, day);
+            }
+            else
+                schedule.InsertOneAsync(day);
+        }
+
+        public void DeleteSubject(TukhvatullinaDay day, Subject subject)
+        {
+            day.Subjects.Remove(subject);
+            SaveTukhvatullinaDay(day);
+        }
+
+        public TukhvatullinaDay GetTukhvatullinaDay(string day)
+        {
+            return GetTukhvatullinaDay().FirstOrDefault(x => x.DayName == day);
+        }
+
+        public Subject AddNewSubject(TukhvatullinaDay day)
+        {
+            Subject subject = new Subject();
+
+            day.Subjects.Add(subject);
+            SaveTukhvatullinaDay(day);
+
+            return subject;
+        }
+    }
+}
