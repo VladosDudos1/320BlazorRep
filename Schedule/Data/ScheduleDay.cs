b82bd7d1033829756a61eb7428f3f8a1c52aaf16
Даниль Фахрутдинов <danilfahrutdinov357@mail.ru>
2022-05-31 12:10:13 +0300

    pre-finish

---
+++ Schedule/Data/ScheduleDay.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+namespace Schedule.Data
+{
+    public class ScheduleDay
+    {
+        [BsonId]
+        [BsonIgnoreIfDefault]
+        private ObjectId _id { get; set; }
+        public ObjectId Id { get => _id; }
+        public string DayName { get; set; }
+        public List<Subject> Subjects { get; set; }
+
+        public ScheduleDay()
+        {
+            Subjects = new List<Subject>();
+        }
+    }
+}
---
+++ Schedule/Services/ScheduleService.cs
@@ -0,0 +1,58 @@
+﻿using MongoDB.Driver;
+using Schedule.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Schedule.Services
+{
+    public class ScheduleService
+    {
+        public static MongoClient client = new MongoClient("mongodb://localhost");
+        public static MongoDatabaseBase database = (MongoDatabaseBase)client.GetDatabase("Schedule");
+
+        public List<ScheduleDay> GetSchedule()
+        {
+            var schedule = (MongoCollectionBase<ScheduleDay>)database.GetCollection<ScheduleDay>("Schedule");
+            return schedule.Find(x => true).ToList();
+        }
+
+        public List<string> GetDays()
+        {
+            return Enumerable.Range(0, 7).Select(i => ((DayOfWeek)i).ToString()).ToList();
+        }
+
+        public void SaveScheduleDay(ScheduleDay day)
+        {
+            var schedule = (MongoCollectionBase<ScheduleDay>)database.GetCollection<ScheduleDay>("Schedule");
+            if (GetSchedule().FirstOrDefault(x => x.Id == day.Id) != null)
+            {
+                var filter = Builders<ScheduleDay>.Filter.Eq(s => s.Id, day.Id);
+                schedule.ReplaceOneAsync(filter, day);
+            }
+            else
+                schedule.InsertOneAsync(day);
+        }
+
+        public void DeleteSubject(ScheduleDay day, Subject subject)
+        {
+            day.Subjects.Remove(subject);
+            SaveScheduleDay(day);
+        }
+
+        public ScheduleDay GetScheduleDay(string day)
+        {
+            return GetSchedule().FirstOrDefault(x => x.DayName == day);
+        }
+
+        public Subject AddNewSubject(ScheduleDay day)
+        {
+            Subject subject = new Subject();
+
+            day.Subjects.Add(subject);
+            SaveScheduleDay(day);
+
+            return subject;
+        }
+    }
+}
